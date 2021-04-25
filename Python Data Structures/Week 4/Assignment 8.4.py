eb419dd61e7a78a4ada621e55d6b5505d98ee767
Raman Kumar Jha <jharaman400@gmail.com>
2021-04-25 23:48:45 +0530

    Update Assignment 8.4.py

--- Python Data Structures/Week 4/Assignment 8.4.py
+++ Python Data Structures/Week 4/Assignment 8.4.py
@@ -1,3 +1,8 @@
+8.4 Open the file romeo.txt and read it line by line. For each line, split the line into a list of words using the split() method. 
+The program should build a list of words. For each word on each line check to see if the word is already in the list and if not append it to the list.
+When the program completes, sort and print the resulting words in alphabetical order.You can download the sample data at http://www.py4e.com/code3/romeo.txt
+
+
 fname = input("Enter file name: ")
 fh = open(fname)
 lst = list()
@@ -8,3 +13,7 @@ for line in fh:
             
 lst.sort()
 print(lst)
+
+
+Desired Output : ['Arise', 'But', 'It', 'Juliet', 'Who', 'already', 'and', 'breaks', 'east', 'envious', 'fair', 'grief', 'is', 
+                 'kill', 'light', 'moon', 'pale', 'sick', 'soft', 'sun', 'the', 'through', 'what', 'window', 'with', 'yonder']
