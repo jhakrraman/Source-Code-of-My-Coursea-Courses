22a94904bb25135ca4b6fb1be285605f83dd9c14
Raman Kumar Jha <jharaman400@gmail.com>
2021-04-25 23:11:52 +0530

    Update Assignment 6.5.py

--- Python Data Structures/Week 1/Assignment 6.5.py
+++ Python Data Structures/Week 1/Assignment 6.5.py
@@ -1,4 +1,11 @@
+6.5 Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below.
+Convert the extracted value to a floating point number and print it out.
+
+
 text = "X-DSPAM-Confidence:    0.8475"
 pos = text.find("0")
 slice = float(text[pos:pos+6])
 print(slice)
+
+
+Desired Output : 0.8475
