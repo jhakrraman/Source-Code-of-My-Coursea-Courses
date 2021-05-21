faf5e6cf62b3e36b1c8d37878e24b491c7ce2979
Raman Kumar Jha <jharaman400@gmail.com>
2021-05-21 18:15:41 +0530

    Create Module 1 Graded Assessment.py

---
+++ Crash Course on Python by Google/Week 1/Module 1 Graded Assessment.py
@@ -0,0 +1,45 @@
+Question 6
+Write a Python script that outputs "Automating with Python is fun!" to the screen.
+
+print("Automating with Python is fun!")
+
+Question 7
+Fill in the blanks so that the code prints "Yellow is the color of sunshine".
+
+color = "Yellow"
+thing = "sunshine"
+print(color + " is the color of " + thing)
+
+Desired Output : Yellow is the color of sunshine
+  
+Question 8
+Keeping in mind there are 86400 seconds per day, write a program that calculates how many seconds there are in a week, if a week is 7 days.  
+Print the result on the screen.   Note: Your result should be in the format of just a number, not a sentence.
+  
+seconds = 7 * (24 * 3600)
+print(seconds)
+
+Desired Output : 604800
+  
+Question 9
+Use Python to calculate how many different passwords can be formed with 6 lower case English letters.  For a 1 letter password,
+there would be 26 possibilities.  For a 2 letter password, each letter is independent of the other, so there would be 26 times 26 possibilities.  
+Using this information, print the amount of possible passwords that can be formed with 6 letters.
+
+letter = 26 ** 6
+print(letter)
+
+Desired Output : 308915776
+  
+10.
+Question 10
+Most hard drives are divided into sectors of 512 bytes each.  Our disk has a size of 16 GB. Fill in the blank to calculate how many sectors the disk has.
+Note: Your result should be in the format of just a number, not a sentence.
+  
+disk_size = 16*1024*1024*1024
+sector_size = 512
+sector_amount = disk_size//sector_size
+
+print(sector_amount)
+
+Desired Output : 33554432
