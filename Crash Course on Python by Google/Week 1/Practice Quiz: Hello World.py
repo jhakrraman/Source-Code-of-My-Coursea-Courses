d370b5dbf38e288f6428648bf05480979545666d
Raman Kumar Jha <jharaman400@gmail.com>
2021-05-21 18:25:28 +0530

    Create Practice Quiz: Hello World.py

---
+++ Crash Course on Python by Google/Week 1/Practice Quiz: Hello World.py
@@ -0,0 +1,11 @@
+Question 4
+Output a message that says "Programming in Python is fun!" to the screen.
+
+print("Programming in Python is fun!")
+
+Question 5
+Replace the ___ placeholder and calculate the Golden ratio:  \frac{1+\sqrt{5}}{2} 
+Tip: to calculate the square root of a number xx, you can use x**(1/2).
+
+ratio = (1 + (5)**(1/2))/2
+print(ratio)
