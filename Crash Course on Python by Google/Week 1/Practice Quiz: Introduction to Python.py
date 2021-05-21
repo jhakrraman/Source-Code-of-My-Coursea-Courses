bb6be68954ce645f344876a2d04028fcf850a5dc
Raman Kumar Jha <jharaman400@gmail.com>
2021-05-21 18:27:02 +0530

    Create Practice Quiz: Introduction to Python.py

---
+++ Crash Course on Python by Google/Week 1/Practice Quiz: Introduction to Python.py
@@ -0,0 +1,21 @@
+Question 1
+Fill in the correct Python command to put “My first Python program” onto the screen.
+ 
+print("My first Python program")
+
+Question 3
+Convert this Bash command into Python:
+
+print("Have a nice day")
+
+Question 4
+Fill in the correct Python commands to put “This is fun!” onto the screen 5 times. 
+
+for i in range(5):
+  print("This is fun!")
+  
+Question 5
+Select the Python code snippet that corresponds to the following Javascript snippet:
+
+for i in range(10):
+  print(i)
